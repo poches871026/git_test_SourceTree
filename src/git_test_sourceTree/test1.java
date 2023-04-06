8d9b89683d1fc48c73550763357de967d0064162
yuhs <poches871026@gmail.com>
2023-04-06 22:18:31 +0900

    230406_test_test1

--- src/git_test_sourceTree/test1.java
+++ src/git_test_sourceTree/test1.java
@@ -8,48 +8,26 @@ public class test1 {
 		System.out.println("11111111111111111");
 		System.out.println("11111111111111111");
 		System.out.println("11111111111111111");
+		
 		System.out.println("11111111111111111");
 		System.out.println("11111111111111111");
 		System.out.println("11111111111111111");
 		System.out.println("11111111111111111");
 		System.out.println("11111111111111111");
+		System.out.println("test1");
+		System.out.println("test1");
+		System.out.println("test1");
+		System.out.println("test1");
+		System.out.println("test1");
+		System.out.println("test1");
+		System.out.println("test1");
+		System.out.println("test1");
+		System.out.println("test1");
+		System.out.println("test1");
 		
 		
-		System.out.println("33333333333333333");
-		System.out.println("33333333333333333");
-		System.out.println("33333333333333333");
-		System.out.println("33333333333333333");
 		
 		
-		System.out.println("test11111");	
-		System.out.println("11111111111111111");
-		System.out.println("11111111111111111");
-		System.out.println("11111111111111111");
-		System.out.println("11111111111111111");
-		System.out.println("11111111111111111");
-		System.out.println("11111111111111111");
-		System.out.println("11111111111111111");
-		System.out.println("11111111111111111");
-		
-		
-		System.out.println("33333333333333333");
-		System.out.println("33333333333333333");
-		System.out.println("33333333333333333");
-		System.out.println("33333333333333333");
-		
-		
-		System.out.println("test11111");	
-		System.out.println("test11111");
-		System.out.println("4444444444444444");
-		System.out.println("55555555555555555");
-		System.out.println("aaaaaaaaaaaaaaaa");
-		System.out.println("aaaaaaaaaaaaaaaa");
-		System.out.println("33333333333333333");
-		System.out.println("4444444444444444");
-		System.out.println("55555555555555555");
-		System.out.println("aaaaaaaaaaaaaaaa");
-		System.out.println("aaaaaaaaaaaaaaaa");
-		System.out.println("33333333333333333");
 		
 		
 		
